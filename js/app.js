75a513257146a900a5c1da0378ee3607eae3704f
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-03-22 15:29:11 -0400

    prepending working

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 //		alert("Value: " + $('#item').val());
     // Prevent the form's default submission.
     event.preventDefault();
-   		$("li").prepend( $('#item').val());
+   		$("li").prepend( $('#item').val() + $('#Qty').val() + $('Unit').val());
     // Log the event object for inspectin'
     console.log( event );
 });
