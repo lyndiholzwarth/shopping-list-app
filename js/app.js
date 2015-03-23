04874928c96cbe4933f38eaaba5ceff90b2096cf
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-03-22 20:31:32 -0400

    clear button working

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
 	$( "form" ).on( "submit", function( event ) {
-//		alert("Value: " + $('#item').val());
-    // Prevent the form's default submission.
     event.preventDefault();
-   		$("li").prepend( $('#item').val() + $('#Qty').val() + $('Unit').val());
+   		$("ul").prepend( $('#item').val() + $('#Qty').val() + $('#Unit').val());
     // Log the event object for inspectin'
     console.log( event );
+});
+
+//	$('ul').on('click', 'li', function(event){
+//	  $("li").toggleClass( "seleted" );
+//	  		console.log (event);
+  // fires when any LIs are clicked on
+  // including LIs that aren't on the page when it is initially loaded
+//});
+	$('.top-header').on ('click', '.clear-button', function(event){
+      $( 'ul' ).remove( );
 });
 
 });
