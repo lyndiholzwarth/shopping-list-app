f103958a30a5add6208e40bceca273c7ab615d9b
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-03-26 20:58:50 -0400

    altered add

--- js/app.js
+++ js/app.js
@@ -12,7 +12,6 @@ $(document).ready(function(){
     // Add new row
     $('#list').prepend(
       '<li class="list-content row">\
-        <div class="col-xs-1 col-md-1">\
           <button class="col-xs-1 col-md-1">\
             <div class="no-check">\
               <i class="fa fa-square-o"></i>\
@@ -21,17 +20,16 @@ $(document).ready(function(){
               <i class="fa fa-check-square-o"></i>\
             </div>\
           </button>\
-        </div>\
-        <div class="col-xs-6 col-md-6 list-item">' + itemName +'</div>\
-        <div class="col-xs-1 col-md-1 list-item">' + QtyName +' </div>\
-        <div class="col-xs-2 col-md-1 list-item">' + UnitName +' </div>\
-          <div class="col-xs-1 col-md-1 list-item"></div>\
-          <button class="col-xs-1 col-md-1 list-item box-right">\
-            <i class="fa fa-pencil"></i>\
-          </button>\
-          <button class="col-xs-1 col-md-1 list-item box-right">\
-            <i class="fa fa-trash-o"></i>\
-          </button>\
+          <div class="col-xs-6 col-md-6 list-item">' + itemName +'</div>\
+          <div class="col-xs-1 col-md-1 list-item">' + QtyName +' </div>\
+          <div class="col-xs-2 col-md-1 list-item">' + UnitName +' </div>\
+            <div class="col-xs-1 col-md-1 list-item"></div>\
+            <button class="col-xs-1 col-md-1 list-item box-right">\
+              <i class="fa fa-pencil"></i>\
+            </button>\
+            <button class="col-xs-1 col-md-1 list-item box-right">\
+              <i class="fa fa-trash-o"></i>\
+            </button>\
       </li>');
   });
 
